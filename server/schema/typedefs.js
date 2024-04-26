8db398d953e392e1633671f657b5d2dc588f24fc
michael <mikek222@gmail.com>
2024-04-26 13:30:53 +0300

    added early typeDef carve outs

--- server/schema/typedefs.js
+++ server/schema/typedefs.js
@@ -1,8 +1,28 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Query {
-    hello: String
+    type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    password: String
+  }
+
+  input UserInput {
+    firstName: String!
+    lastName: String!
+    email: String!
+    password: String!
+    teamId: ID
+  }
+
+    type Query {
+    fill out later
+  }
+
+  type Mutation {
+    fill out later
   }
 `;
 
