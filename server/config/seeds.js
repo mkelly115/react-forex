36a7c68c6f4d8748625d01ed1306c1d6e941c2c4
michael <mikek222@gmail.com>
2024-05-02 17:53:03 +0300

    updated seed data - testing current structure successful

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -7,28 +7,51 @@ const usersData = [
     lastName: 'Doe',
     email: 'john@example.com',
     password: 'password123',
-    balance: { value: '100', currency: 'USD', change: '0' },
-    currentCurrency: { value: '100', currency: 'USD', change: '0' }
+    balance: { value: 100, currency: 'USD', change: 0 },
+    currentCurrency: { value: 100, currency: 'USD', change: 0 }
   },
   {
     firstName: 'Jane',
     lastName: 'Doe',
     email: 'jane@example.com',
     password: 'password456',
-    balance: { value: '200', currency: 'EUR', change: '0' },
-    currentCurrency: { value: '200', currency: 'EUR', change: '0' }
+    balance: { value: 200, currency: 'EUR', change: 0 },
+    currentCurrency: { value: 200, currency: 'EUR', change: 0 }
   },
+  {
+    firstName: 'Alice',
+    lastName: 'Smith',
+    email: 'alice@example.com',
+    password: 'password789',
+    balance: { value: 150, currency: 'GBP', change: 0 },
+    currentCurrency: { value: 150, currency: 'GBP', change: 0 }
+  },
+  {
+    firstName: 'Bob',
+    lastName: 'Johnson',
+    email: 'bob@example.com',
+    password: 'passwordABC',
+    balance: { value: 300, currency: 'USD', change: 0 },
+    currentCurrency: { value: 300, currency: 'USD', change: 0 }
+  }
 ];
+
+
+mongoose.connect('mongodb://localhost:27017/react-forex', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function seedUsers() {
   try {
-    await mongoose.connection.dropDatabase(); // Drop existing database
-    await User.insertMany(usersData); // Insert seed data
+    await User.deleteMany();
+
+    await User.insertMany(usersData);
+
     console.log('Seed data inserted successfully');
   } catch (error) {
     console.error('Error seeding users:', error);
   } finally {
-    mongoose.connection.close(); // Close MongoDB connection
+
+    mongoose.disconnect();
   }
 }
 
